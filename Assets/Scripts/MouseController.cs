f57e84ff155422d7a73b001ee2b2c6cacd84122a
Thomas Mathieson <thomas.mathieson02@gmail.com>
2022-10-14 22:43:27 +0100

    Updated mouse controller

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -7,8 +7,15 @@ public class MouseController : MonoBehaviour
     [SerializeField] private float moveSpeed = 0.01f;
     [SerializeField] private Camera mainCamera;
     [SerializeField] private bool constrainY = true;
+    [SerializeField] private float angleSnaps = 8;
+    [SerializeField] private float turnTime = 0.3f;
 
     private float initialY;
+    private float lastTurn = 0;
+    private float currentAngle = 0;
+    private MouseWall currentWall;
+
+    private readonly string EDGE_WALL_TAG = "EdgeWall";
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +25,40 @@ public class MouseController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    { 
+    {
         // Find what we are pointing at
-        Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit);
+        if (lastTurn >= turnTime)
+        {
+            lastTurn = 0;
+            Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit);
 
-        Vector3 target = new Vector3(hit.point.x, constrainY ? initialY : hit.point.y, hit.point.z);
+            Vector3 target = new Vector3(hit.point.x, constrainY ? initialY : hit.point.y, hit.point.z);
+            Vector3 vec = target - transform.position;
+            currentAngle = Mathf.Round(Mathf.Atan2(vec.z, vec.x) / 2 / Mathf.PI * angleSnaps) / angleSnaps * Mathf.PI * 2;
+        }
+
 
         // Move towards it
-        transform.position = Vector3.MoveTowards(transform.position, Vector3.Lerp(transform.position, target, 0.5f), moveSpeed);
+        Vector3 forward = new Vector3(Mathf.Cos(currentAngle), 0, Mathf.Sin(currentAngle));
+        transform.position += moveSpeed * Time.deltaTime * forward;
+        transform.rotation = Quaternion.LookRotation(forward, Vector3.up);
+
+        lastTurn += Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag(EDGE_WALL_TAG))
+        {
+            currentWall = GetComponent<MouseWall>();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(EDGE_WALL_TAG))
+        {
+            currentWall = null;
+        }
     }
 }
---
+++ Assets/Scripts/MouseWall.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseWall : MonoBehaviour
+{
+    [Range(0f, Mathf.PI*2)]
+    public float exitDirection = 0;
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawRay(transform.position, new Vector3(Mathf.Cos(exitDirection), 0, Mathf.Sin(exitDirection));
+    }
+}
